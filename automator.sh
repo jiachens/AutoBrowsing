842f2fb00495ac0e2f4686a9593b3238582358fd
Jiachen Sun <jiachens@umich.edu>
2019-03-06 15:05:52 -0500

    log to local

--- automator.sh
+++ automator.sh
@@ -3,12 +3,14 @@ function test(){
 	COUNTER=0
 	while [ $COUNTER -lt $1 ]; do 
 		adb shell am instrument -w -r   -e debug false jiachens.umich.edu.autobrowsing.test/androidx.test.runner.AndroidJUnitRunner
+		adb shell logcat -d | grep "PLT" &> $COUNTER'.log'
+		adb shell logcat -c
 		adb shell am force-stop com.android.chrome
-		for element in `adb shell su 0 ls /data/user/0/com.android.chrome/`
+		for element in `adb shell su -c ls /data/user/0/com.android.chrome/`
 		do
 			if [ "$element" != "lib" ] && [ "$element" != "shared_prefs" ]
 			then
-				adb shell su 0 rm -rf /data/user/0/com.android.chrome/$element
+				adb shell su -c rm -rf /data/user/0/com.android.chrome/$element
 			fi
 		done 
 		let COUNTER=COUNTER+1 
@@ -17,13 +19,13 @@ function test(){
 
 # first clear cache
 adb shell am force-stop com.android.chrome
-for element in `adb shell su 0 ls /data/user/0/com.android.chrome/`
+for element in `adb shell su -c ls /data/user/0/com.android.chrome/`
 do
 	if [ "$element" != "lib" ] && [ "$element" != "shared_prefs" ]
 	then
-		adb shell su 0 rm -rf /data/user/0/com.android.chrome/$element
+		adb shell su -c rm -rf /data/user/0/com.android.chrome/$element
 	fi
 done 
-
+adb shell logcat -c
 # $1 stands for the test times
 test $1
